0aa02074b1eb31821257a07e1157d7cc0a36b7eb
DmytroChekun <snashd4@gmail.com>
2020-05-01 14:34:32 +0300

    finished

---
+++ precache-manifest.ee578af621a8cb4d462a01080bda635e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3353272deeaba63ad0ffb6f3f6e23ed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1120b4ac75ab4286ddbf",
+    "url": "/static/css/main.62049d34.chunk.css"
+  },
+  {
+    "revision": "c6e87821d5aeb58d05b1",
+    "url": "/static/js/2.25106022.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/static/js/2.25106022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1120b4ac75ab4286ddbf",
+    "url": "/static/js/main.3d62ceed.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
